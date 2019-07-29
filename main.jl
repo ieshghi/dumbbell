d3bdc17c9d00644de0864755e9edd8004700be66
ieshghi <irajeshghi@gmail.com>
2019-07-29 12:17:57 -0400

    runsim() changed

--- main.jl
+++ main.jl
@@ -63,12 +63,8 @@ using DelimitedFiles
         return out
     end
 
-    function sprgpot(x1,x2,k,l=0) #potential gradient ON PARTICLE 1. For particle 2 take the negative of this
-        if l == 0 #l sets the maximum size of the spring, if it's 0 then the spring is infinitely flexible
-            return k*(x1-x2);
-        else        
-            return k*(x1-x2)/abs((1-((x1-x2)/l)^2));
-        end
+    function sprgpot(x1,x2,k,l) #potential gradient ON PARTICLE 1. For particle 2 take the negative of this
+            return k*(x1-x2-l)#l sets the rest size of the spring, if it's 0 then the spring is infinitely flexible
     end
 
     function xdot(x1,x2,g,k,u,grav,pottype,l)
@@ -76,55 +72,20 @@ using DelimitedFiles
         return force./g;
     end
 
-    function rhs(x,t,grav,pottype,g,t1,t2,l,k)
+    function rhs(x,t,grav,pottype,g,t1,t2,l,k,u)
         x1 = x[1];
         x2 = x[2];
         tr = (t1+t2)/2;
         k = k*tr/50; #make the spring such that if l (l=10) is the period of the potential, then .5kl^2 = Tr
-        
-        u = 1.;
 
         #                                    #
         #   Change physical parameters here  #
         #                                    #
  
-        return xdot(x1,x2,g,k,u,grav,pottype,l);
+        return xdot(x1,x2,g,k,u,grav,pottype,l*10);#multiply l by 10 because we've set the potential period to 10
     end
 
-    function rkint(x,t,dt,grav,pottype,g,l,k) 
-        h = dt;
-        tm = t;
-        k1 = h.*rhs(x,tm,grav,pottype,g,t1,t2,l,k);
-        k2 = h.*rhs((x+k1*1/2),(tm+h*1/2),grav,pottype,g,t1,t2,l,k);
-        k3 = h.*rhs((x+k2*1/2),(tm+h*1/2),grav,pottype,g,t1,t2,l,k);
-        k4 = h.*rhs((x+k3),(tm+h),grav,pottype,g,t1,t2,l,k);
-        x += 1/6*(k1 + 2*k2 + 2*k3 + k4);
-        tm = tm+h;
-        ynew = x;
-        tnew = tm;
-        return ynew,tnew
-    end
-
-
-    function rk_evolve_sde(x1i,x2i,dt,nsteps,t1,t2,grav,pottype,l,k)
-        x = zeros(nsteps,2);
-        t = zeros(nsteps);
-        x[1,:] = [x1i,x2i];
-
-        #                                   #
-        #             And Here              #
-        #                                   #
-
-        g = .1;
-        b = [sqrt(2*t1*dt/g),sqrt(2*t2*dt/g)];
-        for i = 2:nsteps
-            dw = randn(2);
-            x[i,:],t[i] = rkint(x[i-1,:],t[i-1],dt,grav,pottype,g,t1,t2,l,k) ;
-            x[i,:] += b.*dw;
-        end
-        return x,t
-    end
-    function euler_evolve_sde(x1i,x2i,dt,nsteps,t1,t2,grav,pottype,l,k)
+    function euler_evolve_sde(x1i,x2i,dt,nsteps,t1,t2,grav,pottype,l,k,u)
         x = zeros(nsteps,2);
         t = zeros(nsteps);
         x[1,:] = [x1i,x2i];
@@ -136,40 +97,17 @@ using DelimitedFiles
         g = .1;
         b = [sqrt(2*t1*dt/g),sqrt(2*t2*dt/g)]; 
         for i = 2:nsteps
-            a = rhs(x[i-1,:],t[i-1],grav,pottype,g,t1,t2,l,k);
+            a = rhs(x[i-1,:],t[i-1],grav,pottype,g,t1,t2,l,k,u);
             dw = randn(2);
             x[i,:] = x[i-1,:] + a*dt + b.*dw;            
         end
         return x,t
     end
 
-    function manypart(N,x1i,x2i,dt,nsteps,t1,t2,grav,pottype = 0,l=0,k=1)
-        xs = @DArray[rk_evolve_sde(x1i,x2i,dt,nsteps,t1,t2,grav,pottype,l,k)[1] for j=1:N];
+    function manypart_euler(N,x1i,x2i,dt,nsteps,t1,t2,grav = 0,pottype = 0,l=0,k=1,u=1)
+        xs = @DArray[euler_evolve_sde(x1i,x2i,dt,nsteps,t1,t2,grav,pottype,l,k,u)[1] for j=1:N];
         q = sum(xs)./N;
         return q,xs
-    end
-
-    function manypart_euler(N,x1i,x2i,dt,nsteps,t1,t2,grav,pottype = 0,l=0,k=1)
-        xs = @DArray[euler_evolve_sde(x1i,x2i,dt,nsteps,t1,t2,grav,pottype,l,k)[1] for j=1:N];
-        q = sum(xs)./N;
-        return q,xs
-    end
-
-    function manypart_euler_dnv(N,x1i,x2i,dt,nsteps,t1,t2,grav,pottype = 0,l=0,k=1) #decorrelated noise variables
-        xs = @DArray[euler_evolve_sde(x1i,x2i,dt,nsteps,t1,t2,grav,pottype,l,k)[1] for j=1:N];
-        q = sum(xs)./N;
-        alpha = t2/(t1+t2);
-        beta = t1/(t1+t2);
-        
-        xa = q[:,1];
-        xb = q[:,2];
-
-        r = xa-xb;
-        R = alpha*xa+beta*xb;
-
-        q_out = hcat(r,R);
-
-        return q_out,xs
     end
 
     function linfit(xdat,ydat)
@@ -181,6 +119,21 @@ using DelimitedFiles
         intercept = coeff_pred[2];
 
         return slope, intercept
+    end
+
+    function getuscale(umax,kmult = 1)
+        u = LinRange(0,umax,100);
+#        dts = 1.;
+        sl = zeros(100,1);
+        t1 = 0.1;
+        t2 = 1.;
+        tim = LinRange(0,1000000*0.01,1000000);
+        for i = 1:size(u,1);
+            print("U = $(u[i]), finding drift.\n");
+            q,xs = manypart_euler(20,0,1,.01,1000000,t1,t2,0,2,0,kmult,u[i]);
+            sl[i],inter = linfit(tim[100:end],q[100:end,2]);
+        end
+        return sl
     end
 
     function gettscale(tmin)
@@ -228,8 +181,6 @@ using DelimitedFiles
         return sl
     end
 
-
-
     function getk(kvals)
         sl = zeros(size(kvals));
         t1 = .05;
@@ -242,6 +193,19 @@ using DelimitedFiles
         end
         return sl
 
+    end
+
+    function getl(lvals)
+        sl = zeros(size(lvals));
+        t1 = .1;
+        t2 = 1;
+        tim = LinRange(0,10000000*0.001,10000000);
+        for i = 1:size(lvals,1);
+            print("T1 = $(t1), T2 = $(t2), k = $(lvals[i]).\n");
+            q,xs = manypart_euler(50,0,1,.001,10000000,t1,t2,0,2,lvals[i],1);
+            sl[i],inter = linfit(tim[100:end],q[100:end,2]);
+        end
+        return sl
     end
 
     function getgrav(pot,tt,tmin,grange,whatpart=0)
@@ -306,43 +270,28 @@ using DelimitedFiles
     function runsim()
         maxtime = 10000;
         timestep = 0.001;
-        nsteps = Int(maxtime/timestep);
-		#sl_hotgrav_dt0 = getgrav(2,0,0.5,[0.0,.025])
-		#sl_hotgrav_dt1 = getgrav(2,1,0.5,[0.0,.025])
-		#sl_hotgrav_dt2 = getgrav(2,2,0.5,[0.0,.025])
-		#sl_hotgrav_dt3 = getgrav(2,3,0.5,[0.0,.025])
-        #writedlm("./data/sl_hotgrav_dt0",sl_hotgrav_dt0);
-        #writedlm("./data/sl_hotgrav_dt1",sl_hotgrav_dt1);
-        #writedlm("./data/sl_hotgrav_dt2",sl_hotgrav_dt2);
-        #writedlm("./data/sl_hotgrav_dt3",sl_hotgrav_dt3);
-		#		
-		#sl_coldgrav_dt0 = getgrav(2,0,0.05,[0.0,.2])
-		#sl_coldgrav_dt1 = getgrav(2,1,0.05,[0.0,.2])
-		#sl_coldgrav_dt2 = getgrav(2,2,0.05,[0.0,.2])
-		#sl_coldgrav_dt3 = getgrav(2,3,0.05,[0.0,.2])
-        #writedlm("./data/sl_coldgrav_dt0",sl_coldgrav_dt0);
-        #writedlm("./data/sl_coldgrav_dt1",sl_coldgrav_dt1);
-        #writedlm("./data/sl_coldgrav_dt2",sl_coldgrav_dt2);
-        #writedlm("./data/sl_coldgrav_dt3",sl_coldgrav_dt3);
+        
+        #want to test dependencies at high kmult, so that the patching solution is correct:
+#        sl_dt4 = gettscale_spring(0.1,4);
+#        writedlm("./data/parab_strongk/sl_dt4",sl_dt4);
+#        sl_dt3 = gettscale_spring(0.1,3);
+#        writedlm("./data/parab_strongk/sl_dt3",sl_dt3);
+#        sl_dt2 = gettscale_spring(0.1,2);
+#        writedlm("./data/parab_strongk/sl_dt2",sl_dt2);
+#        sl_dt1 = gettscale_spring(0.1,1);
+#        writedlm("./data/parab_strongk/sl_dt1",sl_dt1);
 
-		#sl_cold_moregrav_dt0 = getgrav(2,0,0.05,[0.0,2.0])
-		#sl_cold_moregrav_dt02 = getgrav(2,.2,0.05,[0.0,2.0])
-		#sl_cold_moregrav_dt05 = getgrav(2,.5,0.05,[0.0,2.0])
-		#sl_cold_moregrav_dt1 = getgrav(2,1,0.05,[0.0,2.0])
-        #writedlm("./data/sl_cold_moregrav_dt0",sl_cold_moregrav_dt0);
-        #writedlm("./data/sl_cold_moregrav_dt1",sl_cold_moregrav_dt1);
-        #writedlm("./data/sl_cold_moregrav_dt02",sl_cold_moregrav_dt02);
-        #writedlm("./data/sl_cold_moregrav_dt05",sl_cold_moregrav_dt05);
-
-        #sl_stiffspr = getk(LinRange(1,40,200));
-        #sl_softspr = getk(LinRange(.01,1,200));
-        #writedlm("./data/sl_stiffspr",sl_stiffspr);
-        #writedlm("./data/sl_softspr",sl_softspr);
-
-		#sl_lowt_gr = gettscale_grav(0.1,.01)
-        #writedlm("./data/sl_lowt_gr",sl_lowt_gr);
-		sl_lowt_stiff = gettscale_spring(0.1,35)
-        writedlm("./data/sl_lowt_stiff_parab",sl_lowt_stiff);
+        sl_u5 = getuscale(5,5);
+        writedlm("./data/parab_strongk/sl_u5",sl_u5);
+        sl_u4 = getuscale(5,4);
+        writedlm("./data/parab_strongk/sl_u4",sl_u4);
+        sl_u3 = getuscale(5,3);
+        writedlm("./data/parab_strongk/sl_u3",sl_u3);
+        sl_u2 = getuscale(5,2);
+        writedlm("./data/parab_strongk/sl_u2",sl_u2);
+        sl_u1 = getuscale(5,1);
+        writedlm("./data/parab_strongk/sl_u1",sl_u1);
+        
     end
 
 end
