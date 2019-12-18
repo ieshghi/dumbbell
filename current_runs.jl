cb453bff410331c9b74806c0d66460c02a81d4d4
Iraj Eshghi <ieshghi@github.com>
2019-12-17 23:43:18 -0500

    it works now

--- current_runs.jl
+++ current_runs.jl
@@ -5,12 +5,12 @@ pyplot()
 using HDF5
 
 function calcnet(x,y,p,jx,jy,pars)
-	dx = unique(x)[2]-unique(x)[1];
-	dy = unique(y)[2]-unique(y)[1];
+	dx = x[1,2]-x[1,1] 
+	dy = y[2,1]-y[1,1]
 	jleft = jx[x.== dx];
 	jright = jx[x.== 1-dx];
-
-	return dy*sum(jleft[jleft.<0]+jright[jright.>0])
+	
+	return dy*(sum(jleft[jleft.<0])+sum(jright[jright.>0]))
 end
 
 function prep_vals()
@@ -33,24 +33,23 @@ end
 function compare_vals()
     
     thvals = LinRange(0.1,1,40)
-    v0 = h5read["/home/data/ie355/Documents/code/dumbbell/data/temp_runs/decoup_k1_t0.h5", "vvals"]
-    v1 = h5read["/home/data/ie355/Documents/code/dumbbell/data/temp_runs/decoup_k1_t01.h5", "vvals"]
+    v0 = h5read("/home/ieshghi/Documents/code/dumbbell/data/temp_runs/decoup_k1_t0.h5", "vvals")
+    v1 = h5read("/home/ieshghi/Documents/code/dumbbell/data/temp_runs/decoup_k1_t01.h5", "vvals")
     
-
     thvals_curr = [0.1,0.3,0.5,0.8,1.1]
     ncurr = length(thvals_curr)
     v0_curr = zeros(ncurr);
     v1_curr = zeros(ncurr);
     
     for i = 1:ncurr
-        x,y,p,jx,jy = current.loadstuff(string("extra/tc",0,"th",th[i]))
+        x,y,p,jx,jy,pars = current.loadstuff(string("extra/tc",0,"th",thvals_curr[i]))
         v0_curr[i] = calcnet(x,y,p,jx,jy,pars)
-        x,y,p,jx,jy = current.loadstuff(string("extra/tc",0.01,"th",th[i]))
+        x,y,p,jx,jy,pars = current.loadstuff(string("extra/tc",0.01,"th",thvals_curr[i]))
         v1_curr[i] = calcnet(x,y,p,jx,jy,pars)
     end
 
-    plot(thvals,v0,"-")
-    plot(thvals_curr,v0_curr,".")
+    plot([thvals,thvals_curr],[v0,v0_curr*10^4],reuse=false)
+    plot([thvals,thvals_curr],[v1,v1_curr*10^3],reuse=false)
 
 end
 
