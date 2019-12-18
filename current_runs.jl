8acbf651ab75778963a32099b8352b0003a7e748
ieshghi <irajeshghi@gmail.com>
2019-12-17 23:06:02 -0500

    added comparison code

--- current_runs.jl
+++ current_runs.jl
@@ -1,5 +1,7 @@
 module current_runs
 include("current.jl")
+using Plots
+pyplot()
 using HDF5
 
 function calcnet(x,y,p,jx,jy,pars)
@@ -16,7 +18,7 @@ function prep_vals()
 	tc = 0
 	thvals = [0.1,0.3,0.5,0.8,1.1];
 	for i = thvals
-		pars = [tc,th,1.0,0.3,0,2]
+		pars = [tc,i,1.0,0.3,0,2]
 		x,y,p,jx,jy = current.getrun(10,pars,100,100,3,.01,10^7)
 		current.savestuff(x,y,p,jx,jy,pars,string("extra/tc",tc,"th",i))
 	end
@@ -26,6 +28,31 @@ function prep_vals()
 		x,y,p,jx,jy = current.getrun(10,pars,100,100,3,.01,10^7)
 		current.savestuff(x,y,p,jx,jy,pars,string("extra/tc",tc,"th",i))
 	end
-	h5write("/home/ieshghi/Documents/code/dumbbell/data/hists/extra/params.h5","pars",[10,pars,100,100,3,.01,10^7])
 end
+
+function compare_vals()
+    
+    thvals = LinRange(0.1,1,40)
+    v0 = h5read["/home/data/ie355/Documents/code/dumbbell/data/temp_runs/decoup_k1_t0.h5", "vvals"]
+    v1 = h5read["/home/data/ie355/Documents/code/dumbbell/data/temp_runs/decoup_k1_t01.h5", "vvals"]
+    
+
+    thvals_curr = [0.1,0.3,0.5,0.8,1.1]
+    ncurr = length(thvals_curr)
+    v0_curr = zeros(ncurr);
+    v1_curr = zeros(ncurr);
+    
+    for i = 1:ncurr
+        x,y,p,jx,jy = current.loadstuff(string("extra/tc",0,"th",th[i]))
+        v0_curr[i] = calcnet(x,y,p,jx,jy,pars)
+        x,y,p,jx,jy = current.loadstuff(string("extra/tc",0.01,"th",th[i]))
+        v1_curr[i] = calcnet(x,y,p,jx,jy,pars)
+    end
+
+    plot(thvals,v0,"-")
+    plot(thvals_curr,v0_curr,".")
+
+end
+
+
 end
