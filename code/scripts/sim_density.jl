5ed171aeb84b5ab675d9f369143675f649423cd0
Iraj Eshghi <ieshghi@github.com>
2020-01-15 10:44:30 -0500

    script made

--- code/scripts/sim_density.jl
+++ code/scripts/sim_density.jl
@@ -1,4 +1,4 @@
-n = ARGS[1] #number of timesteps
+n = parse(Int,ARGS[1]) #number of timesteps
 
 include("../base_sim.jl")
 using HDF5
@@ -6,3 +6,8 @@ using HDF5
 pars = [0.1,1.0,1.0,0.7,0];
 l,x,y = simbase.run_and_bin([0,0].*1.0,.01,pars,n,100,100,5.0);
 
+h5open("../../data/hists/lastrun.h5","w") do file
+	write(file,"l",l)
+	write(file,"x",x)
+	write(file,"y",y)
+end
