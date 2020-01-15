b8c4169195398501d572a155197cc6c618cbdd9c
Iraj Eshghi <ieshghi@github.com>
2020-01-15 10:32:19 -0500

    debugged I think?

--- code/dual_current.jl
+++ code/dual_current.jl
@@ -12,17 +12,16 @@
 
 module dual_current
 include("base_sim.jl")
-using Interpolations
 
 function gen_dual_lat(lat::Array{Float64,2},x::Array{Float64,1},y::Array{Float64,1})
 	n = length(x)
-	xn = Array(range(0.,stop = 1.0,length = n+1)[1:end-1]) #This works when n is a multiple of 10 and the patching point is at a multiple of 0.1. No need to generalize further.
-														   #These constraints are already enforced in the base_sim module.
+	xn = Array(range(0.,stop = 1.0,length = n+1)[1:end-1]) #This works when n is a multiple of 10 and the patching point is at a multiple of 0.1. No need to generalize further. These constraints are already enforced in the base_sim module.
 
-	lat_n = 1/2*(lat + circshift(lat,[1,0]))#Interpolate the probability distribution onto the new lattice. Do it row by row because the y coordinates of the new lattice are unchanged.
+	lat_n = 1/2*(lat + circshift(lat,[1,0]))#Interpolate the probability distribution onto the new lattice. At every point, the new lattice values are the average of the old lattice neighbour values.
 	
 	dy = y[2]-y[1]
 	dx = xn[2]-xn[1]
+	
 	lat_dy = 1/(2*dy)*(circshift(lat_n,[0,-1])-circshift(lat_n,[0,1])) #Calculate derivatives with second-order centered finite difference. Assume periodicity (technically untrue in y direction but the probability distribution is basically 0 at large y values so this shouldn't matter.
 	lat_dx = 1/(dx)*(lat-circshift(lat,[1,0])) #This is the slope at the midpoint between two old lattice points.
 	
@@ -30,11 +29,12 @@ function gen_dual_lat(lat::Array{Float64,2},x::Array{Float64,1},y::Array{Float64
 end
 
 function force(x::Array{Float64,1},xo::Array{Float64,1},y::Array{Float64,1},l::Float64,k::Float64)
-	newf = simbase.ext_force.(x,l)
-	newf[1] = 1/2*(newf[end]+newf[2])
+	oldf = simbase.ext_force.(xo,l) #External force on old lattice
+	newf = simbase.ext_force.(x,l) #External force on new lattice (has one discontinuity at 0!)
+	newf[1] = 1/2*(oldf[end]+oldf[1]) #the only problematic point for the force, the discontinuity at 0, is smoothed by taking the value of the force there to be the average of the neighbours on the old lattice 
 
 	xf = -newf*ones(size(y')) + k*ones(size(x))*y'
-	yf = newf*ones(size(y')) - 2*k*ones(size(x))*y' #The rest is just given by langevin equations
+	yf = newf*ones(size(y')) - 2*k*ones(size(x))*y' #Force fields on new lattice as given by Langevin equations
 
 	return xf,yf
 end
