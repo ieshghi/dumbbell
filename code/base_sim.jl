cba559e73fcaa44fb2a708793b4ca6e68ae7da8c
Iraj Eshghi <ieshghi@github.com>
2020-01-28 11:30:27 -0500

    double temperature fixed

--- code/base_sim.jl
+++ code/base_sim.jl
@@ -32,9 +32,8 @@ function ext_force(x::Float64,l::Float64)
 end
 
 function timestep!(x::Array{Float64,1},dt::Float64,pars::Array{Float64,1},noise::Array{Float64,1},ifext::Int)
-	b = sqrt.(dt*[pars[1],pars[2]])
+	b = sqrt.(2*dt*[pars[1],pars[2]])
 	a = rhs(x,pars,ifext)
-	x[1] += a[1]*dt+b[1]*noise[1]
 	hop = a[1]*dt+b[1]*noise[1]
 	x[1] += hop
 	x[2] += a[2]*dt+b[2]*noise[2]-b[1]*noise[1]
@@ -45,10 +44,10 @@ function rhs(x::Array{Float64,1},pars::Array{Float64,1},ifext::Int)
 	l = pars[4]
 	k = pars[3]
 	g = pars[5]	
-	fex = ext_force(x[1],l)*[-1,1]
+	fex = ifext*ext_force(x[1],l)*[-1,1]
 	fspr = k*x[2].*[1,-2]
 	fg = g*[1,0]
-	return ifext*fex+fspr+fg
+	return fex+fspr+fg
 end
 
 function run_and_bin(x0::Array{Float64,1},dt::Float64,pars::Array{Float64,1},nsteps::Int,nx::Int,ny::Int,ymax::Float64,ifext::Int=1)
@@ -58,7 +57,6 @@ function run_and_bin(x0::Array{Float64,1},dt::Float64,pars::Array{Float64,1},nst
 	x = x0
 	maxhop = 0
 	@inbounds for i = 1:nsteps
-			timestep!(x,dt,pars,noise[i,:],ifext)
 			hop = timestep!(x,dt,pars,noise[i,:],ifext)
 			place_in_lattice!(x,lat,xvals,yvals)
 			maxhop += abs(hop)
--- code/dual_current.jl
+++ code/dual_current.jl
@@ -24,9 +24,10 @@ function gen_dual_lat(lat::Array{Float64,2},x::Array{Float64,1},y::Array{Float64
 	dx = xn[2]-xn[1]
 	
 	#lat_dy = 1/(2*dy)*(circshift(lat_n,[0,-1])-circshift(lat_n,[0,1])) #Calculate derivatives with second-order centered finite difference. Assume periodicity (technically untrue in y direction but the probability distribution is basically 0 at large y values so this shouldn't matter.
+
 	lat_dx = 1/(dx)*(lat-circshift(lat,[1,0])) #This is the slope at the midpoint between two old lattice points.
-	
     lat_dy = specder(lat_n,dy,2)
+
     #lat_dx = specder(lat_n,dx,1)
     #lat_dx = (oldlat_dx+circshift(oldlat_dx,[1,0]))./2
 
