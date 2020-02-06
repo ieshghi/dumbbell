b95dc27569f9e452da50799451b54c85f4a5b06d
Iraj Eshghi <ieshghi@github.com>
2020-02-05 23:34:42 -0500

    we can do it now I think

---
+++ code/current_temp_dep.jl
@@ -0,0 +1,44 @@
+module current_temp_dep
+using HDF5
+include("dual_current.jl")
+function get_temps()
+	l = [[] for i = 1:10]
+	push!(l[1],h5read("../data/hists/collected_01.h5","l"))
+	push!(l[2],h5read("../data/hists/collected_02.h5","l"))
+	push!(l[3],h5read("../data/hists/collected_03.h5","l"))
+	push!(l[4],h5read("../data/hists/collected_04.h5","l"))
+	push!(l[5],h5read("../data/hists/collected_05.h5","l"))
+	push!(l[6],h5read("../data/hists/collected_06.h5","l"))
+	push!(l[7],h5read("../data/hists/collected_07.h5","l"))
+	push!(l[8],h5read("../data/hists/collected_08.h5","l"))
+	push!(l[9],h5read("../data/hists/collected_09.h5","l"))
+	push!(l[10],h5read("../data/hists/collected_10.h5","l"))
+	
+	x = h5read("../data/hists/collected_10.h5","x")
+	y = h5read("../data/hists/collected_10.h5","y")
+
+	ts = 0.1*(1:10)
+	jvals = zeros(10)
+	for i = 1:10
+		pars = [0.1,ts[i],1,0.7,0]
+		jx,jy,ln,xn,y,fx,fy,lx,ly = dual_current.calc_curr(l[i],pars,x,y,1)
+		jxo,xo,yo = dual_current.coarse_grain_2(jx,x,y,4)
+		sumj = zeros(size(jxo[1]))
+		dy = y[2]-y[1]
+		for j = 1:size(jxo)[1]
+			sumj[j] = dy*traprule(jxo[j,:])
+		end
+	end
+
+	return sumj
+end
+
+function traprule(a::Array{Float64,1})
+	s = (a[1]+a[end])/2
+	n = length(a)
+	for i = 2:(n-1)
+		s += a[i]
+	end
+
+	return s
+end
--- code/dual_current.jl
+++ code/dual_current.jl
@@ -14,22 +14,20 @@ include("base_sim.jl")
 using DSP 
 using FFTW
 
-function gen_dual_lat(lat::Array{Float64,2},x::Array{Float64,1},y::Array{Float64,1})
+function gen_dual_lat(lat::Array{Float64,2},x::Array{Float64,1},y::Array{Float64,1},l::Float64)
 	n = length(x)
 	xn = Array(range(0.,stop = 1.0,length = n+1)[1:end-1]) #This works when n is a multiple of 10 and the patching point is at a multiple of 0.1. No need to generalize further. These constraints are already enforced in the base_sim module.
 
 	lat_n = 1/2*(lat + circshift(lat,[1,0]))#Interpolate the probability distribution onto the new lattice. At every point, the new lattice values are the average of the old lattice neighbour values.
-	
 	dy = y[2]-y[1]
 	dx = xn[2]-xn[1]
-	
-	lat_dy = 1/(2*dy)*(circshift(lat_n,[0,-1])-circshift(lat_n,[0,1])) #Calculate derivatives with second-order centered finite difference. Assume periodicity (technically untrue in y direction but the probability distribution is basically 0 at large y values so this shouldn't matter.
 
-	lat_dx = 1/(dx)*(lat-circshift(lat,[1,0])) #This is the slope at the midpoint between two old lattice points.
-    #lat_dy = specder(lat_n,dy,2)
+	#lat_dy = 1/(2*dy)*(circshift(lat_n,[0,-1])-circshift(lat_n,[0,1])) #Calculate derivatives with second-order centered finite difference. Assume periodicity (technically untrue in y direction but the probability distribution is basically 0 at large y values so this shouldn't matter.
+    lat_dy = specder(lat_n,dy,2)
 
-    #oldlat_dx = specder(lat_n,dx,1)
-    #lat_dx = (oldlat_dx+circshift(oldlat_dx,[1,0]))./2
+	lat_dx = 4/(3*dx)*(lat-circshift(lat,[1,0])) - 1/(12*dx)*(circshift(lat,[-2,0])-circshift(lat,[2,0])) #This is the slope at the midpoint between two old lattice points.
+	ns = Int(floor(l*length(x)))+1
+	lat_dx[ns] = 1/(2*dx)*(lat[ns]-lat[ns-1])
 
 	return lat_n,lat_dx,lat_dy,xn,y
 end
@@ -38,10 +36,11 @@ function force(x::Array{Float64,1},xo::Array{Float64,1},y::Array{Float64,1},l::F
 	oldf = simbase.ext_force.(xo,l) #External force on old lattice
 	newf = simbase.ext_force.(x,l) #External force on new lattice (has one discontinuity at 0!)
 	newf[1] = 1/2*(oldf[end]+oldf[1]) #the only problematic point for the force, the discontinuity at 0, is smoothed by taking the value of the force there to be the average of the neighbours on the old lattice 
+	ns = Int(floor(l*length(x)))+1
+	newf[ns] = 1/2*(oldf[ns-1]+oldf[ns]) #the only problematic point for the force, the discontinuity at 0, is smoothed by taking the value of the force there to be the average of the neighbours on the old lattice 
 
 	xf = -ifext*newf*ones(size(y')) + k*ones(size(x))*y'
 	yf = ifext*newf*ones(size(y')) - 2*k*ones(size(x))*y' #Force fields on new lattice as given by Langevin equations
-
 	return xf,yf
 end
 
@@ -52,9 +51,9 @@ function calc_curr(c::Array{Float64,2},pars::Array{Float64,1},xo::Array{Float64,
 	t2 = pars[2]
 	D = [[t1,-t1],[-t1,t1+t2]]
 
-	cn,cdx,cdy,xn,y = gen_dual_lat(c,xo,y)
+	cn,cdx,cdy,xn,y = gen_dual_lat(c,xo,y,l)
 	fx,fy = force(xn,xo,y,l,k,ifext)
-	
+
 	jx = fx.*cn.-D[1][1].*cdx.-D[1][2].*cdy #See Fokker-Planck equation for the langevin equations.
 	jy = fy.*cn.-D[2][1].*cdx.-D[2][2].*cdy
 
